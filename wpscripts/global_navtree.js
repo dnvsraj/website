c47146553401694a24cb0d044516996b4925de6b
dnvsraj <dnvsraj@redifmail.com.com>
2017-01-17 06:31:40 +0530

    new update

---
+++ wpscripts/global_navtree.js
@@ -0,0 +1,98 @@
+var global_navtree = WpNavBar.readTree({
+"childArray" : [
+{   'sTitle':'Home',
+    'bIsWebPath':true,
+    'sUrl':'index.html',"childArray" : [
+    {   'sTitle':'Devops Online Classes Course syllabus',
+        'bIsAnchor':true,
+        'bIsWebPath':true,
+        'sUrl':'index.html#DEVOPS-Course-Content'
+    },
+    {   'sTitle':'Amazon web services cousre Syllubas',
+        'bIsAnchor':true,
+        'bIsWebPath':true,
+        'sUrl':'index.html#AWS-Course-Content'
+    },
+    {   'sTitle':'IBM AIX Administration online Training Course content',
+        'bIsAnchor':true,
+        'bIsWebPath':true,
+        'sUrl':'index.html#aix-administration-training-course-content'
+    },
+    {   'sTitle':'Veritas Volume Manager Course Content',
+        'bIsAnchor':true,
+        'bIsWebPath':true,
+        'sUrl':'index.html#vxvm-Administration-course-content'
+    },
+    {   'sTitle':'Veritas Cluster Administration Course content',
+        'bIsAnchor':true,
+        'bIsWebPath':true,
+        'sUrl':'index.html#vcs-administration-course-content'
+    }]
+},
+{   'sTitle':'Aboutus',
+    'bIsWebPath':true,
+    'sUrl':'devops-training-aboutus.html'
+},
+{   'sTitle':'DevOps',
+    'bIsWebPath':true,
+    'sUrl':'devops-online-training-from-india.html',"childArray" : [
+    {   'sTitle':'What is DevOps, what are the Advantages',
+        'bIsAnchor':true,
+        'bIsWebPath':true,
+        'sUrl':'devops-online-training-from-india.html#What-is-DevOps'
+    },
+    {   'sTitle':'definitions for DevOps ',
+        'bIsAnchor':true,
+        'bIsWebPath':true,
+        'sUrl':'devops-online-training-from-india.html#DevOps-definitions'
+    },
+    {   'sTitle':'How Many Teams Involved in Devops',
+        'bIsAnchor':true,
+        'bIsWebPath':true,
+        'sUrl':'devops-online-training-from-india.html#Teams-Involved-in-Devops'
+    },
+    {   'sTitle':'devOps and Software Development Life Cycle',
+        'bIsAnchor':true,
+        'bIsWebPath':true,
+        'sUrl':'devops-online-training-from-india.html#devOps-software-Development-Life-Cycle'
+    },
+    {   'sTitle':'DevOps and Software Development Life Cycle',
+        'bIsAnchor':true,
+        'bIsWebPath':true,
+        'sUrl':'devops-online-training-from-india.html#DevOps-Software-Development-Life-Cycle'
+    },
+    {   'sTitle':'devops Waterfall Model',
+        'bIsAnchor':true,
+        'bIsWebPath':true,
+        'sUrl':'devops-online-training-from-india.html#Waterfall-Model'
+    }]
+},
+{   'sTitle':'AWS',
+    'bIsWebPath':true,
+    'sUrl':'amazon-web-service-aws.html'
+},
+{   'sTitle':'AIX',
+    'bIsWebPath':true,
+    'sUrl':'aix-administration-online-training.html'
+},
+{   'sTitle':'Linux ',
+    'bIsWebPath':true,
+    'sUrl':'rhel-linux-administration-online-training.html'
+},
+{   'sTitle':'VxVM ',
+    'bIsWebPath':true,
+    'sUrl':'veritas-volume-manager-online-training.html'
+},
+{   'sTitle':'VCS',
+    'bIsWebPath':true,
+    'sUrl':'veritas-cluster-administration-online-training.html'
+},
+{   'sTitle':'Software Testing',
+    'bIsWebPath':true,
+    'sUrl':'selenium-qtp-qc-manual-testing-tools.html'
+},
+{   'sTitle':'Registration',
+    'bIsWebPath':true,
+    'sUrl':'devops-online-classes-contacts-.html'
+}]
+});
